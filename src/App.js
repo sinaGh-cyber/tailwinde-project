12188e124b51f56eadec957541d98c9857ec13eb
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-08-10 00:01:11 +0430

    RadioGroup component ready

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import RadioGroup from "./components/RadioGroup/RadioGroup";
+
 export default function App() {
   return (
     <div className="bg-gray-200 flex justify-center items-center min-h-screen ">
-      
+      <RadioGroup/>
     </div>
   );
 }
