7fc823ed2a1cbe71a725ca30a3abdda946f8921e
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-08-09 14:40:35 +0430

    dropDown dynamic ready

--- src/components/HeadLessUi/HeadLessUi.jsx
+++ src/components/HeadLessUi/HeadLessUi.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import ChevronDownSvg from './ChevronDownSvg';
 
 const HeadLessUi = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="relative ">
-      <button className=" px-4 py-2 bg-blue-500 text-white rounded-lg flex justify-center items-center focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2 focus:ring-offset-white">
+      <button
+        onClick={() => {
+          setIsOpen((isOpen) => !isOpen);
+        }}
+        className=" px-4 py-2 z-10 relative bg-blue-500 text-white rounded-lg flex justify-center items-center focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2 focus:ring-offset-white"
+      >
         <span>show more</span>
         <ChevronDownSvg />
       </button>
-      <div className="bg-white w-56 rounded-lg overflow-hidden mt-1 absolute ">
+      <div
+        className={`inset-0  bg-gray-400  ${isOpen ? 'fixed' : 'hidden'}`}
+        onClick={() => {
+          setIsOpen(false);
+        }}
+      ></div>
+      <div
+        className={`bg-white w-56 rounded-lg overflow-hidden mt-1 opacity-70 ${
+          isOpen ? 'absolute' : 'hidden'
+        }`}
+      >
         <a href="#/" className="block p-4 hover:bg-gray-100">
           setting
         </a>
