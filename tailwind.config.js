5c0f1a6f151da57b1187e353472a7efb95499f66
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-08-13 21:48:59 +0430

    coustom color palet aded to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,36 @@ module.exports = {
       animation: {
         'hidden-to-visible': 'hiddenToVisible 0.5s linear',
       },
+      colors: {
+        orange: {
+          100: '#FFCBCB',
+          300: '#FF9F8E',
+          400: '#FF755C',
+          500: '#F25E57',
+          600: '#FC5858',
+          700: '#DE3618',
+        },
+        slate: {
+          700: '#606060',
+          800: '#222F5D',
+          900: '#161D25',
+          200: '#E2E2E2',
+        },
+        stone: {
+          50: '#FDFDFD',
+          100: '#F4F4F4',
+        },
+        gray: {
+          200: '#EBEBEB',
+          300: '#AFAFAF',
+        },
+        indigo: {
+          600: '#8750FB',
+        },
+        yellow: {
+          400: '#FFDE74',
+        },
+      },
     },
   },
   plugins: [],
