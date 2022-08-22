9ff89e1d77b0a89db432f7200b700847926832a3
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-08-22 20:54:24 +0430

    core layput added to project

---
+++ src/components/ProductsLayout/ProductsLayout.jsx
@@ -0,0 +1,22 @@
+import ProductList from '../ProductList/ProductList';
+import SideBar from '../SideBar/SideBar';
+import SortAndFilter from '../SortAndFilter/SortAndFilter';
+
+const ProductsLayout = () => {
+  return (
+    <div className=" grid grid-cols-12 md:grid-rows-[55px,minmax(500px,_1fr)] gap-y-8 md:gap-y-5 ">
+      {' '}
+      <div className=" col-span-full  md:col-span-8 lg:col-span-9 md:px-4">
+        <SortAndFilter />
+      </div>
+      <div className=" hidden md:block col-span-4 lg:col-span-3 row-span-2 bg-slate-50">
+        <SideBar />
+      </div>{' '}
+      <div className=" col-span-full md:col-span-8 lg:col-span-9">
+        <ProductList />
+      </div>
+    </div>
+  );
+};
+
+export default ProductsLayout;
