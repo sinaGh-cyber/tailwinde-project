3ae10dafa825dbecafe2510298fd49b95779255d
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-08-23 20:57:17 +0430

    productionList reStyled

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,7 +3,7 @@ import productsData from './productsData';
 
 const ProductList = () => {
   return (
-    <ul className=" grid  xsm:grid-cols-2  gap-y-9 gap-x-2 md:gap-x-5 md:gap-y-12 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-9 lg:mb-11">
+    <ul className=" grid px-4  xxsm:grid-cols-2 xsm:grid-cols-3  gap-y-8 gap-x-2 md:gap-x-5 md:gap-y-10 md:grid-cols-2 xmd:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-9 lg:mb-11">
       {' '}
       {productsData.map(({ id, ...rest }) => (
         <Product {...rest} key={id} />
