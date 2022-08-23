f236db7c17587ab3d294438a6df5c12bac2b2344
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-08-23 20:58:00 +0430

    tabTypes constant added

---
+++ src/components/SortAndFilter/tabTypes.js
@@ -0,0 +1,5 @@
+export const MOST_Favorite = 'MOST_Favorite';
+export const CHEAPEST = 'CHEAPEST';
+export const MOST_EXPENSIVE = 'MOST_EXPENSIVE';
+export const HOT_SAILING = 'HOT_SAILING';
+// export const MOST_Favorite = 'MOST_Favorite';
